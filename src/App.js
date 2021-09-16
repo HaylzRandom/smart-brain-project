2a918c07ed4009ff9fea29bacbbdfade03c8972d
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2021-09-16 21:55:15 +0100

    Prepped for deployment

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends React.Component {
 
 	onButtonSubmit = () => {
 		this.setState({ imageUrl: this.state.input });
-		fetch('http://localhost:3000/imageurl', {
+		fetch('https://radiant-river-77439.herokuapp.com/imageurl', {
 			method: 'post',
 			headers: {
 				'Content-Type': 'application/json',
@@ -98,7 +98,7 @@ class App extends React.Component {
 			.then((response) => response.json())
 			.then((response) => {
 				if (response) {
-					fetch('http://localhost:3000/image', {
+					fetch('https://radiant-river-77439.herokuapp.com/image', {
 						method: 'put',
 						headers: {
 							'Content-Type': 'application/json',
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -23,7 +23,7 @@ class Register extends react.Component {
 	};
 
 	onSubmitRegister = () => {
-		fetch('http://localhost:3000/register', {
+		fetch('https://radiant-river-77439.herokuapp.com/register', {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({
